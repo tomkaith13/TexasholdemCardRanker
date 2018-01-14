63f33dd9315359e7319f5d667acd05e31e7d2dea
bibinthomas <bibin.thomas.k@gmail.com>
2018-01-14 13:44:59 -0800

    Initial Commit

---
+++ src/main/java/com/bibin/thomas/takehome/TexasHoldemGame.java
@@ -0,0 +1,10 @@
+package com.bibin.thomas.takehome;
+
+/**
+ * Hello world!
+ */
+public class TexasHoldemGame {
+    public static void main(String[] args) {
+        System.out.print("Hello TexasHoldem!");
+    }
+}
---
+++ src/test/java/com/bibin/thomas/takehome/TexasHoldemGameTest.java
@@ -0,0 +1,39 @@
+package com.bibin.thomas.takehome;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Unit test for simple TexasHoldemGame.
+ */
+public class TexasHoldemGameTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+
+    @Test
+    public void testHelloWorld() {
+
+        TexasHoldemGame.main(null);
+        assertEquals("Hello TexasHoldem!", outContent.toString());
+    }
+}
